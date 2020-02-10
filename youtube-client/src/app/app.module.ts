8570f6a7f40f4fe76a0a84d546f1ac6f7665c8fc
Pavel Leskovets <pleskavets@ibagroup.eu>
2020-02-10 17:49:21 +0300

    feat: added service

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { SearchComponent } from "./components/search/search.component";
-import { UserComponent } from "./components/user/user.component";
-import { SearchSettingsComponent } from "./components/search-settings/search-settings.component";
-import { ClipCardComponent } from "./components/clip-card/clip-card.component";
-import { FullInfoCardComponent } from "./components/full-info-card/full-info-card.component";
-import { ClipContainerComponent } from "./components/clip-container/clip-container.component";
-import { LoginFormComponent } from "./components/login-form/login-form.component";
-import { RegistrationFormComponent } from "./components/registration-form/registration-form.component";
-import { NotFoundComponent } from "./components/not-found/not-found.component";
-import { MainPageComponent } from "./components/main-page/main-page.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { UserComponent } from './components/user/user.component';
+import { SearchSettingsComponent } from './components/search-settings/search-settings.component';
+import { ClipCardComponent } from './components/clip-card/clip-card.component';
+import { FullInfoCardComponent } from './components/full-info-card/full-info-card.component';
+import { ClipContainerComponent } from './components/clip-container/clip-container.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,9 +28,15 @@ import { MainPageComponent } from "./components/main-page/main-page.component";
     LoginFormComponent,
     RegistrationFormComponent,
     NotFoundComponent,
-    MainPageComponent
+    MainPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- youtube-client/src/app/components/search/search.component.ts
+++ youtube-client/src/app/components/search/search.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-search",
-  templateUrl: "./search.component.html",
-  styleUrls: ["./search.component.scss"]
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  public ngOnInit(): void {}
 
-  public getClipInfo() {
-    console.log(1);
+  public getClipInfo(event: Event): void {
+    console.log(event)
   }
 }
